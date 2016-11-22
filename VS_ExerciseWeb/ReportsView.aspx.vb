685b350d8324a4824b82d97a5b7510ec397eae76
Glen Simon <simongle@msu.edu>
2016-11-22 17:32:14 -0500

    Toggling Reports now works

--- VS_ExerciseWeb/ReportsView.aspx.vb
+++ VS_ExerciseWeb/ReportsView.aspx.vb
@@ -12,36 +12,21 @@ Partial Class ReportsView
 
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-
-        'ReportViewer1.LocalReport.DataSources.Clear()
-
         Dim reportDataSource = New Microsoft.Reporting.WebForms.ReportDataSource()
-
-        ReportViewer1.LocalReport.ReportEmbeddedResource = "PrescribedWorkouts.rdlc"
-        ReportViewer1.LocalReport.ReportPath = MapPath("PrescribedWorkouts.rdlc")
 
         reportDataSource.Name = "DataSet1"
         reportDataSource.DataSourceId = "ObjectDataSource2"
-        'reportDataSource.Value = DataSetBindingSource
-
-
         ReportViewer1.LocalReport.DataSources.Add(reportDataSource)
 
+        If ReportViewer1.LocalReport.ReportPath = "PrescribedWorkouts.rdlc" Then
+            ReportViewer1.LocalReport.ReportEmbeddedResource = "CompletedWorkouts.rdlc"
+            ReportViewer1.LocalReport.ReportPath = "CompletedWorkouts.rdlc"
+        Else
+            ReportViewer1.LocalReport.ReportEmbeddedResource = "PrescribedWorkouts.rdlc"
+            ReportViewer1.LocalReport.ReportPath = "PrescribedWorkouts.rdlc"
+        End If
+
         ReportViewer1.LocalReport.Refresh()
-
-
-        'ReportViewer1.Reset()
-        ' ReportViewer1.LocalReport.ReportEmbeddedResource = MapPath("PrescribedWorkouts.rdlc") + ".rdlc"
-        'ReportViewer1.LocalReport.ReportEmbeddedResource = "VS_ExerciseWeb.PrescribedWorkouts.rdlc"
-
-
-        'ReportViewer1.LocalReport.ReportPath = MapPath("PrescribedWorkouts.rdlc")
-        'ReportViewer1.LocalReport.LoadReportDefinition()
-
-        'ReportViewer1.DataBind()
-        'ReportViewer1.LocalReport.Refresh()
-
-        'ReportViewer1.LocalReport.Refresh()
 
     End Sub
 
